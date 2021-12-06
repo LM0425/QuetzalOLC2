ad3ea4ae0fb4eba68d59d8142243886fc168f404
LM0425 <0108moralesluis@gmail.com>
2021-12-06 11:12:16 -0600

    integracion jison y codemirror

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function analizar(entrada) {
+    
+    console.log("analizar");
+    console.log(entrada);
+    try {
+        test=gramatica.parse(entrada);
+        console.log("test",test)
+    } catch (error) {
+        console.log(error);   
+    }
+}
+
+
+
